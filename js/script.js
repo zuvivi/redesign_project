29ff269d247c6ae31d456b04a2415aef38644559
zuvivi <chub0070@gmail.com>
2026-03-18 13:01:41 +0900

    260318(수) 수정

--- js/script.js
+++ js/script.js
@@ -23,3 +23,51 @@ function goLogin() {
 function goSetting() {
   alert("환경설정 페이지 입니다.");
 }
+
+const slider = document.querySelector(".music-container");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("dragging");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("dragging");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("dragging");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; // 🔥 속도 조절
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+let isDragging = false;
+
+slider.addEventListener("mousemove", () => {
+  if (isDown) isDragging = true;
+});
+
+slider.addEventListener("mouseup", () => {
+  setTimeout(() => (isDragging = false), 0);
+});
+
+document.querySelectorAll(".music-card").forEach((card) => {
+  card.addEventListener("click", (e) => {
+    if (isDragging) e.preventDefault();
+  });
+});
